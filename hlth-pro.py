21ec603219e040821053febdd1118620eb0a2df7
mo-banzu <56958213+hellterhead@users.noreply.github.com>
2020-04-17 21:51:40 +0700

    Update hlth-pro.py

--- hlth-pro.py
+++ hlth-pro.py
@@ -4,7 +4,7 @@ from HELLTERHEAD import pyimgflip
 import youtube_dl
 from googletrans import Translator
 from bs4 import BeautifulSoup
-import pafy
+from pafy import pafy
 import ffmpy
 import goslate
 import wikipedia
